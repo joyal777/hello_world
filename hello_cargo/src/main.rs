7b6ce44d2b87c030b33f7052fd4e151c2adb89a9
joyal777 <joyaltony78@gmail.com>
2026-06-28 10:40:20 +0530

    learn some concepts

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,29 +1,54 @@
 fn main() {
-    let mut x = 5;
-    println!("{x}");
-    x = 6;
-    println!("{x}");
-    x = 7;
-    println!("{x}");
-    const CONST_SHOULD_NAMED_LIKE_THIS: u32 = 60*60*3;
-    println!("{CONST_SHOULD_NAMED_LIKE_THIS}");
+    // let mut x = 5;
+    // println!("{x}");
+    // x = 6;
+    // println!("{x}");
+    // x = 7;
+    // println!("{x}");
+    // const CONST_SHOULD_NAMED_LIKE_THIS: u32 = 60*60*3;
+    // println!("{CONST_SHOULD_NAMED_LIKE_THIS}");
 
-    let x = 5;
-        let x = x + 1;
-        {
-            let x = x * 40;
-            println!("The value of x in the inner scope is: {x}");
-            if x >= 100 {
-                let x = x * 2;
-                println!("The value of x in the inner inner scope is: {x}");
-            }
+    // let x = 5;
+    //     let x = x + 1;
+    //     {
+    //         let x = x * 40;
+    //         println!("The value of x in the inner scope is: {x}");
+    //         if x >= 100 {
+    //             let x = x * 2;
+    //             println!("The value of x in the inner inner scope is: {x}");
+    //         }
+    //     }
+    //     println!("The value of x is: {x}");
+
+    //     let spaces = "   ";
+    //     let spaces =  spaces.len();
+    //     println!("The value of spaces is: {spaces}");
+
+    //     let guess: f64 = "4562.9".parse().expect("Not a number!");
+    //     println!("The value of guess is: {guess}");
+
+        let sum = 5 + 10;
+        let difference = 95.5 - 4.3;
+
+        let product = 4 * 30;
+
+        let quotient = 56.7 / 32.2;
+        let truncated = -5 / 3;
+
+        let remainder = 43 % 5;
+        println!("The value of sum is: {sum}");
+        println!("The value of difference is: {difference}");
+        println!("The value of product is: {product}");
+        println!("The value of quotient is: {quotient}");
+        println!("The value of truncated is: {truncated}");
+        println!("The value of remainder is: {remainder}");
+
+        let cb = [10,20,30,40,50,60,70];
+        let mut m = 0;
+
+        while m < 5 {
+            println!("the value is: {}", cb[m]);
+            
+            m += 1;
         }
-        println!("The value of x is: {x}");
-
-        let spaces = "   ";
-        let spaces =  spaces.len();
-        println!("The value of spaces is: {spaces}");
-
-        let guess: f64 = "4562.9".parse().expect("Not a number!");
-        println!("The value of guess is: {guess}");
 }
