27b2d08a63ada312e47ced3664bd4fcf0d7dc8b7
joyal777 <joyaltony78@gmail.com>
2026-06-29 22:48:14 +0530

    New Chap Ownership in variable and functions

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -83,3 +83,21 @@ fn main() {
                                         // xb does NOT move into the function,
                                         // so it's okay to use xb afterward.
 }
+fn gives_ownership() -> String {       // gives_ownership will move its
+                                       // return value into the function
+                                       // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                        // some_string is returned and
+                                       // moves out to the calling
+                                       // function
+}
+
+// This function takes a String and returns a String.
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
